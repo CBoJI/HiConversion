8045645a3cd6fe87507bf7c132a68c5c8c39f841
nazarov <aonazarov@gmail.com>
2016-05-27 14:42:35 +0500

    renamed custom_mail, custom_admin to mail, admin

--- app/extensions/__init__.py
+++ app/extensions/__init__.py
@@ -2,7 +2,7 @@
 
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from mail import CustomMail
+from custom_mail import CustomMail
 
 from app.admin import CustomAdmin
 
@@ -10,8 +10,8 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-custom_mail = CustomMail()
+mail = CustomMail()
 db = SQLAlchemy()
-custom_admin = CustomAdmin(db=db)
+admin = CustomAdmin(db=db)
 
-extensions_list = [login_manager, db, custom_admin, custom_mail, ]
+extensions_list = [login_manager, db, admin, mail, ]
